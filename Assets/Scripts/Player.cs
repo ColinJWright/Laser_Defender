a0632ed2ead8ba2cc899b1b4aebcc544ec32a7f6
ColinJWright <colinwright2771@gmail.com>
2021-02-10 19:21:39 +0000

    Reinstated the code for Hit() in DamageDealer, Player & Enemy scripts. With it, the Player will Destroy objects on contact (bad) but lasers also get destroyed (good)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,7 +50,7 @@ public class Player : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
-        //damageDealer.Hit();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Die();
