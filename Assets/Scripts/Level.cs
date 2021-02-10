e2d24cd6d60afd5a8102e9268f4bed3f253c7ea1
ColinJWright <colinwright2771@gmail.com>
2021-02-10 18:35:57 +0000

    Delay For Loading Scene Lesson complete

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] float gameOverDelay = 2f;
+
     public void LoadStartMenu()
     {
         SceneManager.LoadScene(0);
@@ -17,6 +19,12 @@ public class Level : MonoBehaviour
 
     public void LoadGameOver()
     {
+        StartCoroutine(GameOver());
+    }
+
+    IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(gameOverDelay);
         SceneManager.LoadScene("Game Over");
     }
 
