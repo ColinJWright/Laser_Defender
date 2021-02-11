dc27182720b026337167711fe811eb7044bfd622
ColinJWright <colinwright2771@gmail.com>
2021-02-11 18:28:57 +0000

    New Enemy & Projectile Lesson complete

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] int scoreValue = 150;
 
     [Header("Enemy Shooting")]
-    [SerializeField] float shotCounter;
+    float shotCounter;
     [SerializeField] float minTimeBetweenShots = 0.2f;
     [SerializeField] float maxTimeBetweenShots = 3f;
     [SerializeField] GameObject enemyLaserPrefab;
